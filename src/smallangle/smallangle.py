2bf70e2392a01476c87f74ba00dc714bc58968d7
David <david.rebel@outlook.com>
2024-11-15 14:13:19 +0100

    command line commandos aan de praat

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,18 +3,35 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Ammount of values displayed",
+    show_default=True
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Ammount of values displayed",
+    show_default=True
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
-
 if __name__ == "__main__":
-    sin(10)
+    sin()
